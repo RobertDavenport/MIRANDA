4b3b4e0ada97cbfc893b199482c4390dfe7d452d
AlexanderLFloyd <32877587+AlexanderLFloyd@users.noreply.github.com>
2021-03-17 01:51:30 -0500

    Fixed grammatical issues and added comments

--- main/HapticControl/HapticControl.ino
+++ main/HapticControl/HapticControl.ino
@@ -4,20 +4,22 @@ const uint8_t HAPTIC_MOTOR_1 = 3;
 const uint8_t HAPTIC_MOTOR_2 = 4;
 
 void setup() {
-  // Initialize the haptic motor's control pin.
+  // Initialize the haptic motors' control pins.
   pinMode(HAPTIC_MOTOR_1, OUTPUT);
   pinMode(HAPTIC_MOTOR_2, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  // Checks if data is in the Serial buffer
   if(Serial.available()){
     byte intensity = Serial.parseInt();
-    Serial.println(intensity);
-    feedback(intensity);
+    Serial.println(intensity); // Echos the input
+    feedback(intensity); // Update the haptic intensity.
   }
 }
 
+// Writes the passesd intensity to the haptic motors by PWM.
 void feedback(int intensity){
   analogWrite(HAPTIC_MOTOR_1, intensity);
   analogWrite(HAPTIC_MOTOR_2, intensity);
