ec59d51dbbb93880f116492bc0316273e6c2e101
AlexanderLFloyd <32877587+AlexanderLFloyd@users.noreply.github.com>
2021-03-23 14:21:19 -0500

    Added comments

--- main/Arduino/Libraries/TOFArray/TOFArray.cpp
+++ main/Arduino/Libraries/TOFArray/TOFArray.cpp
@@ -1,8 +1,11 @@
 #include <TOFArray.h>
 #include <Arduino.h>
 
+// Default constructor
 TOFArray::TOFArray(){}
 
+// Constructor for sensor array, pass no. sensors, and array of trigger pins, echo pins, and 
+// variables to hold the read distances.
 TOFArray::TOFArray(int numSensors, int *triggerPins, int *echoPins, int *distances) {
 	_numSensors = numSensors;
 	_tPins = triggerPins;
@@ -10,23 +13,25 @@ TOFArray::TOFArray(int numSensors, int *triggerPins, int *echoPins, int *distanc
 	_distances = distances;
 }
 
+// Measure the distances from each sensor
 void TOFArray::readDistance() {
 	for(int i = 0; i < _numSensors; i++) {
 		*(_distances + i) = readDistanceSensor(i);
 	}	
 }
 
-
+// Read the distance from the sensor with index 'sensor'
 // Courtesy of John Saho: [https://create.arduino.cc/projecthub/knackminds/how-to-measure-distance-using-ultrasonic-sensor-hc-sr04-a-b9f7f8]
 int TOFArray::readDistanceSensor(int sensor) {
 	int tPin = *(_tPins + sensor);
 	int ePin = *(_ePins + sensor);
 	
 	digitalWrite(tPin, LOW);
-	delayMicroseconds(2);
+	delayMicroseconds(2); // Broadcast sound for 2 microseconds
 	digitalWrite(tPin, HIGH);
 	delayMicroseconds(10);
-	int res = pulseIn(ePin, HIGH) / 29 / 2;
-	if(res > maxVal || res < minVal){res = -1;};
+	// Read the amount of time it takes for the sound waves to return to the sensor.
+	int res = pulseIn(ePin, HIGH) / 29 / 2; 
+	if(res > maxVal || res < minVal){res = -1;}; // return -1 if value is outside accepted range.
 	return res;
 }
