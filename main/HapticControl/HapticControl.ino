5bb0fe12ef5b18c8ba7d7866762b2023f73c6a51
Jonas Kety <runescape.guy.jk@gmail.com>
2021-04-07 05:15:01 -0500

    profiler for haptic feedback

--- main/HapticControl/HapticControl.ino
+++ main/HapticControl/HapticControl.ino
@@ -1,3 +1,5 @@
+#include <TimeProfiler.h>
+
 char intensity[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 int intensity_values[8] = {0, 130, 155, 175, 190, 205, 220, 255};
 //int pins[5] = {9, 10, 3, 5, 6};
@@ -11,11 +13,18 @@ void setup() {
 }
 
 void loop() {
+  
+  TIMEPROFILE_BEGIN(one); // profiler
+  
   if(Serial.available()){
+    
+    SCOPED_TIMEPROFILE(all); // begin scoped profiler
+    
     int vals[5] = {0,0,0,0,0};
     for(int i = 0; i<5; i++){
       char c = Serial.read();
       Serial.println(c);
+      TIMEPROFILE_END(one); // end "one"
       int j = 0;
       for(j = 0; j < 8; j++){
         if(c == intensity[j])
@@ -23,8 +32,16 @@ void loop() {
       }
       vals[i] = intensity_values[j];
     }
+    
+    // Print profiler
+    Serial.print("all : ");
+    Serial.println(TimeProfiler.getProfile("all");
+    Serial.print("one : ");
+    Serial.println(TimeProfiler.getProfile("one");
+    
     for(int i = 0; i<5; i++){
       analogWrite(pins[i], vals[i]);
     }
+    
   }
 }
