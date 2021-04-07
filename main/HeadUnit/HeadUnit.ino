dc8a0fb5f4f079cbc0b4ad9ae47f9719d181eb16
dtothelove <49038117+dtothelove@users.noreply.github.com>
2021-04-06 19:42:07 -0500

    Added Function to read from lidar array
    Added a function in HeadUnit to read from the array of lidar sensors and update an array of distances

--- main/HeadUnit/HeadUnit.ino
+++ main/HeadUnit/HeadUnit.ino
@@ -2,6 +2,8 @@
 #include <TFLI2C.h>
 #include <Wire.h>
 
+TFLI2C tfli2c;
+
 // Min and max acceptable values for distance read in the from TF LUNA TOF sensor
 #define MAX_DISTANCE 450
 #define MIN_DISTANCE 1
@@ -10,25 +12,53 @@
 #define MIN_MOTOR_SPEED 100
 #define MAX_MOTOR_SPEED 255
 
-int16_t tfDistance; // Holds the recorded distance
-uint8_t tfAddress = 0x10; // Address of the TF LUNA sensor
-uint8_t tfSensor1 = 0x08;
+// Sensor Addresses
+uint8_t TFSENSOR_LEFT = 0x10;
+uint8_t TFSENSOR_MIDDLE = 0x20;
+uint8_t TFSENSOR_RIGHT = 0x21;
+// Sensor Array
+uint8_t sensorArray[3] = {TFSENSOR_LEFT, TFSENSOR_MIDDLE, TFSENSOR_RIGHT};
+
+// Distance Array for sensor Readings
+int16_t distanceReadings[3] = {0,0,0};
+
 unsigned char hapticIntensityOne;
 
 const int TF_LUNA_1 = 10; // Controls the power to the TF Luna sensor
-TFLI2C tfli2c; 
+
+
 
 void setup() {
-  Serial.begin(9600);
+  //pinMode(TF_LUNA_PINOUT, OUTPUT);
+  Serial.begin(115200);
   Wire.begin();
-  digitalWrite(TF_LUNA_1, LOW); // Powers on the TF Luna
+  //digitalWrite(TF_LUNA_1, LOW); // Powers on the TF Luna
 }
 
 void loop() {
-  pinMode(TF_LUNA_1, OUTPUT);
-  tfli2c.getData(tfDistance, tfAddress);
-  Serial.println(feedback(tfDistance));
+  readFromArray(sensorArray, distanceReadings);
+  printDistances(distanceReadings);
   delay(150);
+}
+
+
+
+
+// Gets distance readings for each sensor in the array and updates the array of distances
+void readFromArray(uint8_t *sensorArray, int16_t *distanceReadings) {
+  for(int i = 0; i<3; i++){
+    tfli2c.getData(distanceReadings[i], sensorArray[i]);
+  }
+}
+
+// prints Distance readings to serial
+void printDistances(int16_t *distanceReadings) {
+  Serial.print("Left: ");
+  Serial.println(distanceReadings[0]);
+  Serial.print("Middle: ");
+  Serial.println(distanceReadings[1]);
+  Serial.print("Right: ");
+  Serial.println(distanceReadings[2]);
 }
 
 // Maps the read-in distance value to the range of possible distance values normalized to 
